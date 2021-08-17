ab31c11eba81215add1fffab8fb2c385db6d6ea0
Tolgonai Serkebaeva <asunur2009@gmail.com>
2021-08-17 11:54:35 -0400

    weather icons added, today date added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 // import Sidebar from "./components/Sidebar";
 // import WeatherInfo from "./components/WeatherInfo";
 import {useState} from 'react'
-
+const API_KEY = "93acb782ba9cde069e257374df26c92b"
 
 // fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=93acb782ba9cde069e257374df26c92b&units=metric&lang=ru')
 // .then((res) => res.json())
@@ -14,7 +14,9 @@ function App() {
 
   const [countries, setCountries] = useState([])
 
-  const [loadind, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const [weather, setWeather] =useState(null)
 
   const searchBarClass = "widget__searching " + (searchBar ? "opened" : "")
 
@@ -33,6 +35,24 @@ function App() {
         setLoading(false)
       })
   }
+
+  function getWeather(country){
+    const { lat, lon, address } =country
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=ru`)
+    .then((res)=> res.json())
+    .then((data) =>{ 
+      setWeather({
+        temp: Math.round(data.main.temp),
+        tempFeelsLike: Math.round(data.main.feels_like),
+        description: data.weather[0].description,
+        city: address.city
+
+      })
+      setSearchBar(false)
+    })
+
+  }
+
 
   return (
     <div className="App">
@@ -54,25 +74,31 @@ function App() {
           </button>
           <form className="searching__form" onSubmit={onSearch} >
             <input type="search" className="searching-form__input" type="search" name="query" />
-            <button className=" button search-form__button" type="button" onClick={onSearch} >Найти</button>
+            <button className=" button search-form__button" type="button" type="submit"  >Найти</button>
           </form>
 
-          {loading && (<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>)}
-            <ul>
+          {loading && (<div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>)}
+            <ul className="search-results">
               {countries.map((country) =>
-                <li key={country.id}> {country.display_name} </li>
+                <li key={country.place_id}> 
+                <button onClick={()=>getWeather(country)} > {country.display_name} </button>
+                 </li>
               )}
             </ul>
 
         </div>
         <header className="widget__header"></header>
-        <div className="widget__body">
-          <div className="widget-body__temp">1<span className="temp__space">&nbsp;</span><span className="temp__symbol">°C</span></div>
-          <div className="widget-body__text">Cнег</div>
-          <div className="widget-body__feels-like">Ощущается как -3 °C</div>
+        { weather && (
+           <div className="widget__body">
+          <div className="widget-body__temp">
+            {weather.temp}
+            <span className="temp__space">&nbsp;</span><span className="temp__symbol">°C</span></div>
+          <div className="widget-body__text"> {weather.description} </div>
+          <div className="widget-body__feels-like">
+            Ощущается как {weather.tempFeelsLike} °C </div>
           <div className="widget-body__date">
             <div className="date__text">Сегодня</div>
-            <div className="date__info">Вс, 13 мар</div>
+            <div className="date__info"> Вс, 13 мар  </div>
           </div>
           <div className="widget-body__geo">
             <div className="geo__icon">
@@ -80,9 +106,11 @@ function App() {
                 <path d="M11.9999 0.333344C5.54992 0.333344 0.333252 5.55001 0.333252 12C0.333252 20.75 11.9999 33.6667 11.9999 33.6667C11.9999 33.6667 23.6666 20.75 23.6666 12C23.6666 5.55001 18.4499 0.333344 11.9999 0.333344ZM11.9999 16.1667C10.8949 16.1667 9.83504 15.7277 9.05364 14.9463C8.27224 14.1649 7.83325 13.1051 7.83325 12C7.83325 10.8949 8.27224 9.83513 9.05364 9.05373C9.83504 8.27233 10.8949 7.83334 11.9999 7.83334C13.105 7.83334 14.1648 8.27233 14.9462 9.05373C15.7276 9.83513 16.1666 10.8949 16.1666 12C16.1666 13.1051 15.7276 14.1649 14.9462 14.9463C14.1648 15.7277 13.105 16.1667 11.9999 16.1667Z" fill="#EC6E4D"/>
               </svg>
             </div>
-            <div className="geo_city">Москва</div>
+            <div className="geo_city">{weather.city}</div>
           </div>
         </div>
+        ) 
+      }
       </div>
 
     </div>
